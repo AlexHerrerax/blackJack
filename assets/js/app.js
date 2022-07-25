52cb99ea0be478c783fc7659b700d93cb090e3a3
Alex Herrera <38311783+AlexHerrerax@users.noreply.github.com>
2022-07-25 04:39:29 -0400

    Juego finalizado

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,13 @@ let especiales=["A","J","Q","K"]
 //DOM
 
 const btnPedir = document.querySelector('#btnPedir')
+const btnDetener = document.querySelector('#btnDetener')
+const btnNuevo = document.querySelector('#btnNuevo')
 const jugadorCarta = document.querySelector('#jugador-carta')
+const computadorCarta = document.querySelector('#computador-carta')
 
 let puntosJugador =0
+let puntosComputador =0
 
 
 
@@ -80,13 +84,93 @@ btnPedir.addEventListener('click', () =>{
     nuevaCarta.classList.add('carta')
     jugadorCarta.append(nuevaCarta)
 
-    if(puntosJugador >=21){
+    if(puntosJugador >21 ||puntosJugador===21 ){
         btnPedir.disabled=true
+        btnDetener.disabled=true
+        juegaComputador()
+    
     }
+
+
+    
+
+})
+
+const juegaComputador = () =>{
+
+
+    do {
+        
+        const carta = pedirCarta()
+
+    let valor = obtenerValor(carta)
+    puntosComputador= puntosComputador+valor
+    document.querySelectorAll('small')[1].innerText=puntosComputador
+
+    const nuevaCarta = document.createElement('img');
+    nuevaCarta.src=`/assets/cartas/${carta}.png`
+    nuevaCarta.classList.add('carta')
+    computadorCarta.append(nuevaCarta);
+
+    if(puntosComputador>21){
+    break;
+}
+
+
+    } while ((puntosComputador<=puntosJugador)&&(puntosJugador<21) );
+
+    setTimeout(() => {
+           if(puntosComputador===puntosJugador){
+            alert("Nadie gana")
+           }else if(puntosJugador>21){
+            alert("Perdiste")
+           }else if(puntosComputador>21){
+            alert("Ganaste")
+           }else if(puntosJugador===21){
+            alert("Ganaste")
+           }else{
+            alert("Perdiste")
+           }
+
+    }, 100);
+
+
+   
+}
+
+btnDetener.addEventListener('click', ()=>{
+
+btnPedir.disabled=true;
+btnDetener.disabled=true;
+juegaComputador();
+
+
+
+
 
 })
 
 
+btnNuevo.addEventListener('click', ()=>{
+
+    console.log(deck)
+deck=[];
+crearDeck()
+
+    puntosJugador =0
+puntosComputador =0
+btnPedir.disabled=false;
+btnDetener.disabled=false;
+document.querySelectorAll('small')[0].innerText=0
+
+document.querySelectorAll('small')[1].innerText=0
+
+jugadorCarta.innerHTML=""
+computadorCarta.innerHTML=""
+
+console.log(deck)
+
+})
 
 
 
